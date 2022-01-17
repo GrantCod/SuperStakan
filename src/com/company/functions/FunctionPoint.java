540e00718901543007da9b22c2d13bf7abe95ba0
Анна Черномырдина <50167514+Steeech@users.noreply.github.com>
2022-01-17 22:00:23 +0400

    init commit

---
+++ src/com/company/functions/FunctionPoint.java
@@ -0,0 +1,38 @@
+package com.company.functions;
+
+public class FunctionPoint {
+    private double x;
+    private double y;
+
+    public FunctionPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public FunctionPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public FunctionPoint(FunctionPoint point) {
+        this.x = point.x;
+        this.y = point.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ src/com/company/functions/TabulatedFunction.java
@@ -0,0 +1,61 @@
+package com.company.functions;
+
+public class TabulatedFunction {
+    private double leftX;
+    private double rightX;
+    private FunctionPoint[] array;
+
+    public TabulatedFunction(double leftX, double rightX, int pointsCount) {
+        this.leftX = leftX;
+        this.rightX = rightX;
+        this.array = new FunctionPoint[pointsCount];
+        double step = (rightX - leftX) / pointsCount;
+        double currentX = leftX;
+        for (int i = 0; i < pointsCount; i++) {
+            this.array[i] = new FunctionPoint(currentX, 0);
+            currentX = currentX + step;
+        }
+
+    }
+
+    public TabulatedFunction(double leftX, double rightX, double[] values) {
+        this.leftX = leftX;
+        this.rightX = rightX;
+        int pointsCount = values.length;
+        double currentX = leftX;
+        double step = (rightX - leftX) / pointsCount;
+        this.array = new FunctionPoint[pointsCount];
+        for (int i = 0; i < pointsCount; i++) {
+            this.array[i] = new FunctionPoint(currentX, values[i]);
+            currentX = currentX + step;
+
+        }
+
+
+    }
+
+    public double getLeftDomainBorder() {
+        return this.leftX;
+
+    }
+
+    public double getRightDomainBorder() {
+        return this.rightX;
+    }
+
+    public double getFunctionValue(double x) {
+        if (x >= leftX && x <= rightX) {
+
+            for (int i = 0; i < this.array.length; i++) {
+                if (this.array[i].getX() == x) {
+                    return this.array[i].getY();
+
+                } else if (x <= this.array[i].getX()) {
+                    return this.array[i - 1].getY();
+                }
+            }
+
+        }
+    return Double.NaN;
+    }
+}
